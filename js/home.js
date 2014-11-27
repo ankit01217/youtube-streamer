ee5f9d52a5a715b44f3666b279d1cde67b32e142
ankit01217 <ankit01217@gmail.com>
2014-11-27 23:14:48 +0530

    added video streamming

--- js/home.js
+++ js/home.js
@@ -1,26 +1,19 @@
 
+
 function getListItem(item){
     var thumbnailURL = item.snippet.thumbnails.default.url;
-    //var itemHTML = '<li><a id="video-link" href="#" onclick="onItemClick(\''+item.id.videoId+'\')"><img src="'+thumbnailURL+'"><h2>'+item.snippet.title+'</h2><p>'+item.snippet.description+'</p></a></li>';
-    var itemHTML = '<li><a id="video-link" href="http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v='+item.id.videoId+'" target="_blank"><img src="'+thumbnailURL+'"><h2>'+item.snippet.title+'</h2><p>'+item.snippet.description+'</p></a></li>';
+    var itemHTML = '<li><a id="'+item.id.videoId+'" href="#popupVideo" data-rel="popup" data-position-to="window" onclick="addIframePopup(\''+item.id.videoId+'\')"><img class="thumbImg" height="100%" src="'+thumbnailURL+'"><h2>'+item.snippet.title+'</h2><p>'+item.snippet.description+'</p></a><a href="http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v='+item.id.videoId+'" target="_blank" id="download-btn" class="ui-btn ui-btn-b"></a></li>';
     
     return itemHTML;
 }
 
-function onItemClick(videoId){
-    //alert("on item click "+videoId);
+
+function addIframePopup(videoId){
+    //alert("addIframePopup");
     sessionStorage.videoId = videoId;
 
-    $(document).on("pagebeforeshow", "#preview",function(){
-        playVideo();
-
-    });
-
-    // Change page
-    $.mobile.changePage("#preview");
-
-
 }
+
 
 $(document).on('keydown', '#search-1', function(event) {
     // This function wil be called.
@@ -47,5 +40,64 @@ $(document).on('keydown', '#search-1', function(event) {
     }
 
 });
+
+
+
+// popup functions
+$( document ).on( "pagecreate", function() {
+    // The window width and height are decreased by 30 to take the tolerance of 15 pixels at each side into account
+    function scale( width, height, padding, border ) {
+        var scrWidth = $( window ).width() - 30,
+            scrHeight = $( window ).height() - 30,
+            ifrPadding = 2 * padding,
+            ifrBorder = 2 * border,
+            ifrWidth = width + ifrPadding + ifrBorder,
+            ifrHeight = height + ifrPadding + ifrBorder,
+            h, w;
+        if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
+            w = ifrWidth;
+            h = ifrHeight;
+        } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
+            w = scrWidth;
+            h = ( scrWidth / ifrWidth ) * ifrHeight;
+        } else {
+            h = scrHeight;
+            w = ( scrHeight / ifrHeight ) * ifrWidth;
+        }
+        return {
+            'width': w - ( ifrPadding + ifrBorder ),
+            'height': h - ( ifrPadding + ifrBorder )
+        };
+    };
+    $( ".ui-popup iframe" )
+        .attr( "width", 0 )
+        .attr( "height", "auto" );
+    $( "#popupVideo" ).on({
+        popupbeforeposition: function() {
+            //alert('before open');
+            $("#youtube-iframe").attr("src","http://www.youtube.com/embed/"+sessionStorage.videoId+"?autoplay=1");
+
+            // call our custom function scale() to get the width and height
+            var size = scale( $( window ).width() *0.6, $( window ).height() *(0.6*4/3), 10, 1 ),
+                w = size.width,
+                h = size.height;
+            $( "#popupVideo iframe" )
+                .attr( "width", w )
+                .attr( "height", h );
+        },
+        popupafteropen:function(){
+            //$("#youtube-iframe").;
+
+        },
+        popupafterclose: function() {
+            $("#youtube-iframe").attr("src","");
+
+            $( "#popupVideo iframe" )
+                .attr( "width", 0 )
+                .attr( "height", 0 );
+        }
+    });
+});
+
 
 
